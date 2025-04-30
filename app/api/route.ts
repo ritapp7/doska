60bca5b6e5b2e2e847932ebbcaa74128182c4ef6
Студент группы IS <is@sielom.local>
2025-04-30 10:29:53 +0000

    gigachat

--- app/api/route.ts
+++ app/api/route.ts
@@ -20,16 +20,16 @@ export async function POST(req) {
   const body = await req.json();
   const supabase = await createClient();
 
-  const get_users = async () => {
-    const { data: users = [] } = await supabase.from("users").select("*");
+  const get_user_1 = async () => {
+    const { data: users = [] } = await supabase.from("users_1").select("*");
     return users;
   }
 
   try {
     const resp = await client.chat({
-      messages: [{role: "system", content: "Ты умеешь получать список пользователей при помощи функции get_users"}, { role: 'user', content: body.message }],
+      messages: [{role: "system", content: "Ты умеешь получать список пользователей при помощи функции get_users_1"}, { role: 'user_1', content: body.message }],
       functions: [{
-        name: 'get_users',
+        name: 'get_users_1',
         description: 'Получает список всех пользователей',
         parameters: {
           type: 'object',
@@ -46,8 +46,8 @@ export async function POST(req) {
 
     if (resp.choices[0].message.function_call) {
       const functionToCall = resp.choices[0].message.function_call.name;
-      if (functionToCall === "get_users") {
-        const users = await get_users();
+      if (functionToCall === "get_users_1") {
+        const users = await get_users_1();
         return NextResponse.json({ type: "func", message: users, status: 200})
       }
     } else {
